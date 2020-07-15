0453c708bc2a83994b2570e9145f4917bb7e3a3f
denglish357 <denglish357@gmail.com>
2020-07-15 09:49:47 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+  <b-nav>
+    <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+    <b-nav-item disabled>Link</b-nav-item>
+  </b-nav>
+</div>
+</template>
+
---
+++ src/components/questionBox.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="question-box-container">
+        <div>
+            <b-jumbotron>
+                <template v-slot:lead>
+         {{ currentQuestion.question }}
+</template>
+
+    <hr class="my-4">
+
+
+    <b-list-group>
+  <b-list-group-item v-for="(answer, index) in answers" :key="index" @lick="selectAnswer(index)">
+  {{answer}}
+  </b-list-group-item >
+</b-list-group>
+    
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button variant="success" href="#" @click="next">Next</b-button>
+  </b-jumbotron>
+</div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: {
+        currentQuestion: Object,
+        next: Function
+    },
+    computed: {
+        answers() {
+            let answers = [...this.currentQuestion.incorrect_answers]
+            answers.push(this.currentQuestion.corrrect_answer)
+            return answers
+        }
+    },
+    methods: {
+        selectAnswer(index) {
+            console.log(index);
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+.list-group {
+    margin-bottom: 15px;
+}
+
+.btn {
+    margin: 0 5px;
+}
+</style>
